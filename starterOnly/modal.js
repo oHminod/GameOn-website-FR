4587901db45b3ad9829ed1972f595699bc97cec8
oHminod <yvesm13@gmail.com>
2024-04-11 16:23:47 +0200

    form validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,19 +30,39 @@ const lastName = document.getElementById("last");
 const email = document.getElementById("email");
 const birthdate = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
-const location1 = document.getElementsByName("location1");
-const location2 = document.getElementsByName("location2");
-const location3 = document.getElementsByName("location3");
-const location4 = document.getElementsByName("location4");
-const location5 = document.getElementsByName("location5");
-const location6 = document.getElementsByName("location6");
+const location1 = document.getElementById("location1");
+const location2 = document.getElementById("location2");
+const location3 = document.getElementById("location3");
+const location4 = document.getElementById("location4");
+const location5 = document.getElementById("location5");
+const location6 = document.getElementById("location6");
 const terms = document.getElementById("checkbox1");
+
+const nameRegex = /^[a-zA-Z]{2,}([a-zA-Z]*[' -]?[a-zA-Z]+)*$/;
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const birthdateRegex =
+    /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
+const quantityRegex = /^(?!$)\d+$/;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // close modal event
 closeBtn.addEventListener("click", closeModal);
+
+// inputs validation events
+firstName.addEventListener("input", validateFirstName);
+lastName.addEventListener("input", validateLastName);
+email.addEventListener("input", validateEmail);
+birthdate.addEventListener("change", validateBirthdate);
+quantity.addEventListener("input", validateQuantity);
+location1.addEventListener("change", validateLocation);
+location2.addEventListener("change", validateLocation);
+location3.addEventListener("change", validateLocation);
+location4.addEventListener("change", validateLocation);
+location5.addEventListener("change", validateLocation);
+location6.addEventListener("change", validateLocation);
+terms.addEventListener("change", validateTerms);
 
 // launch modal form
 function launchModal() {
@@ -54,9 +74,187 @@ function closeModal() {
     modalbg.style.display = "none";
 }
 
+function validateFirstName() {
+    const existingError = firstNameWrapper.querySelector(".error");
+    if (existingError) {
+        existingError.remove();
+    }
+    if (!nameRegex.test(firstName.value)) {
+        if (existingError) {
+            existingError.remove();
+        }
+        firstName.style.border = "2px solid red";
+        const error = document.createElement("p");
+        error.classList.add("error");
+        error.innerHTML =
+            "Veuillez entrer un prénom valide. (Au moins deux caractères)";
+        firstNameWrapper.appendChild(error);
+        return false;
+    } else {
+        firstName.style.border = "2px solid transparent";
+        return true;
+    }
+}
+
+function validateLastName() {
+    const existingError = lastNameWrapper.querySelector(".error");
+    if (existingError) {
+        existingError.remove();
+    }
+    if (!nameRegex.test(lastName.value)) {
+        if (existingError) {
+            existingError.remove();
+        }
+        lastName.style.border = "2px solid red";
+        const error = document.createElement("p");
+        error.classList.add("error");
+        error.innerHTML =
+            "Veuillez entrer un nom valide. (Au moins deux caractères)";
+        lastNameWrapper.appendChild(error);
+        return false;
+    } else {
+        lastName.style.border = "2px solid transparent";
+        return true;
+    }
+}
+
+function validateEmail() {
+    const existingError = emailWrapper.querySelector(".error");
+    if (existingError) {
+        existingError.remove();
+    }
+    if (!emailRegex.test(email.value)) {
+        if (existingError) {
+            existingError.remove();
+        }
+        email.style.border = "2px solid red";
+        const error = document.createElement("p");
+        error.classList.add("error");
+        error.innerHTML = "Veuillez entrer une adresse email valide.";
+        emailWrapper.appendChild(error);
+        return false;
+    } else {
+        email.style.border = "2px solid transparent";
+        return true;
+    }
+}
+
+function validateBirthdate() {
+    const existingError = birthdateWrapper.querySelector(".error");
+    if (existingError) {
+        existingError.remove();
+    }
+    if (!birthdateRegex.test(birthdate.value)) {
+        if (existingError) {
+            existingError.remove();
+        }
+        birthdate.style.border = "2px solid red";
+        const error = document.createElement("p");
+        error.classList.add("error");
+        error.innerHTML = "Veuillez entrer une date de naissance valide.";
+        birthdateWrapper.appendChild(error);
+        return false;
+    } else {
+        birthdate.style.border = "2px solid transparent";
+        return true;
+    }
+}
+
+function validateQuantity() {
+    const existingError = quantityWrapper.querySelector(".error");
+    console.log("quantity", quantityRegex.test(quantity.value));
+    if (existingError) {
+        existingError.remove();
+    }
+    if (!quantityRegex.test(quantity.value)) {
+        if (existingError) {
+            existingError.remove();
+        }
+        quantity.style.border = "2px solid red";
+        const error = document.createElement("p");
+        error.classList.add("error");
+        error.innerHTML = "Veuillez entrer un nombre valide.";
+        quantityWrapper.appendChild(error);
+        return false;
+    } else {
+        quantity.style.border = "2px solid transparent";
+        return true;
+    }
+}
+
+function validateLocation() {
+    const checkedLocations = [
+        location1.checked,
+        location2.checked,
+        location3.checked,
+        location4.checked,
+        location5.checked,
+        location6.checked,
+    ];
+
+    const oneLocationIsChecked =
+        checkedLocations.filter((location) => location).length === 1;
+
+    const existingError = locationWrapper.querySelector(".error");
+    if (existingError) {
+        existingError.remove();
+    }
+
+    if (!oneLocationIsChecked) {
+        if (existingError) {
+            existingError.remove();
+        }
+        const error = document.createElement("p");
+        error.classList.add("error");
+        error.innerHTML = "Veuillez choisir une ville.";
+        locationWrapper.appendChild(error);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validateTerms() {
+    const checkedTerms = terms.checked;
+
+    const existingError = termsWrapper.querySelector(".error");
+    if (existingError) {
+        existingError.remove();
+    }
+
+    if (!checkedTerms) {
+        if (existingError) {
+            existingError.remove();
+        }
+        const error = document.createElement("p");
+        error.classList.add("error");
+        error.innerHTML = "Veuillez accepter les conditions d'utilisation.";
+        termsWrapper.appendChild(error);
+        return false;
+    } else {
+        return true;
+    }
+}
+
 function validate(e) {
     e.preventDefault();
-    if (true) {
+    const isFirstNameValid = validateFirstName();
+    const isLastNameValid = validateLastName();
+    const isEmailValid = validateEmail();
+    const isBirthdateValid = validateBirthdate();
+    const isQuantityValid = validateQuantity();
+    const isLocationValid = validateLocation();
+    const isTermsValid = validateTerms();
+
+    if (
+        isFirstNameValid &&
+        isLastNameValid &&
+        isEmailValid &&
+        isBirthdateValid &&
+        isQuantityValid &&
+        isLocationValid &&
+        isTermsValid
+    ) {
         form.innerHTML = "";
         form.classList.add("success");
         const text = document.createElement("p");
